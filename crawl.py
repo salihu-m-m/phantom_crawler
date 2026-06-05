588bc9ede2ab66ccc6eb41af8de722a5bb22612a
salihu-m-m <salihumuhammed606@gmail.com>
2026-06-05 13:13:36 +0100

    crawling now works althouch crtl C not yet handled

--- crawl.py
+++ crawl.py
@@ -1,7 +1,9 @@
+from urllib import response
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup, Tag
 from urllib.parse import urlparse, urljoin
 from typing import TypedDict
+import requests
 
 class PageData(TypedDict):
     url: str
@@ -63,3 +65,46 @@ def extract_page_data(html: str, base_url: str) -> PageData:
         "outgoing_links": get_urls_from_html(html, base_url),
         "image_urls": get_images_from_html(html, base_url)  
     }
+def get_html(url: str) -> str:
+
+    try:
+        response = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"}, timeout=10)
+    
+        if response.status_code >= 400:
+            raise ValueError(f"Error fetching {url}: HTTP {response.status_code}")
+        if "text/html" not in response.headers.get("content-Type", ""):
+            raise ValueError(f"Error fetching {url}: content-Type is not text/html")
+    except requests.exceptions.RequestException as e:
+        raise ValueError(f"Error fetching {url}: {e}")
+    return response.text
+def crawl_page(base_url, current_url=None, page_data=None):
+    if current_url is None:
+        current_url = base_url
+    if page_data is None:
+        page_data = {}
+    base_parsed = urlparse(base_url)
+    current_parsed = urlparse(current_url)
+    if current_parsed.netloc != base_parsed.netloc:
+        return page_data
+    normalized_current_url = normalize_url(current_url) 
+    if normalized_current_url in page_data:
+        return page_data
+    try:
+        html = get_html(current_url)
+    except Exception as e:
+        print(f"Error occurred while fetching {current_url}: {e}")
+        return page_data
+    print(f"html type: {type(html)}, value: {html[:100] if html else html}")
+
+    
+    page_data[normalized_current_url] = extract_page_data(html, current_url)
+    for link in page_data[normalized_current_url]["outgoing_links"]:
+        if link not in page_data:
+            page_data =crawl_page(base_url, link, page_data)
+    return page_data    
+        
+    
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+import crawl
+
+
 def main():
-    print("Hello from phantom-crawler!")
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    
+    print(f"starting crawl of {sys.argv[1]}")
+    page_data = crawl.crawl_page(sys.argv[1])
+    print(f"found {len(page_data)} pages")
+    for page in page_data.values():
+        print(page)
+
 
 
 if __name__ == "__main__":
